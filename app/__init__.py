896ef3494b52fc48baf3dd8ffd8cfc2480f8ea3d
Csaba Nagy <csabanagy@shaw.ca>
2025-11-22 20:15:11 -0700

    11

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from .database import init_db
-from .nvr import init_nvr
 
 def create_app():
     app = Flask(__name__)
@@ -10,7 +9,7 @@ def create_app():
 
     # Init database (creates schema if missing)
     init_db(app)
-    init_nvr()  # Initialize NVR (if needed)
+
 
     # Register blueprints
     from .routes import bp as routes_bp
